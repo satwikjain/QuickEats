7938580f94506a828aca5c6cfd9b0603eaa06aa9
Satwik Jain <Satwikjain25@gmail.com>
2023-12-25 22:48:54 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,6 @@ app.use('/api', require("./Routes/DisplayData"))
 app.use('/api', require("./Routes/OrderData"))
 app.use('/api', require("./Routes/myorderData"))
 
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-
